d6d4be38c21bf5ef9fc1e92aa087e4c04c2b72c3
GameDevBaiyi <gamedevbaiyi@gmail.com>
2026-05-03 13:48:59 +0800

    补充遗漏提交

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -4,7 +4,9 @@ namespace Core
 {
     public class GameManager : SingletonMono<GameManager>
     {
-        protected override void OnAwake()
+        protected override void OnAwake() { }
+
+        private void Start()
         {
             BuildingManager.Instance.Init();
             DynamicUIManager.Instance.InitUIs(BuildingManager.Instance.Buildings);
